75420308c4a02d5c372cb157617e44f804aedb69
SHE1UV <126342687+SHE1UV@users.noreply.github.com>
2025-08-24 12:19:01 +0200

    Update models.py

--- geoinfostore/models.py
+++ geoinfostore/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+
 class Address(models.Model):
-    raw_address = models.CharField('Адрес', max_length=255, unique=True)
+    raw_address = models.CharField(
+        'Адрес',
+        max_length=255,
+        unique=True
+    )
     latitude = models.DecimalField(
         'Широта',
         max_digits=9,
@@ -16,7 +21,10 @@ class Address(models.Model):
         null=True,
         blank=True
     )
-    last_updated = models.DateTimeField('Последнее обновление', auto_now=True)
+    last_updated = models.DateTimeField(
+        'Последнее обновление',
+        auto_now=True
+    )
 
     def __str__(self):
         return f"{self.raw_address} ({self.latitude}, {self.longitude})"
