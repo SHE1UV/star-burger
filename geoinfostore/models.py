8d3c28abf3988e71b57e9276899855bc7aa3e059
SHE1UV <126342687+SHE1UV@users.noreply.github.com>
2025-08-24 12:10:48 +0200

    Update models.py

--- geoinfostore/models.py
+++ geoinfostore/models.py
@@ -1,49 +1,22 @@
 from django.db import models
 
-class GeocodingAddresses(models.Model):
-    first_address = models.CharField(
-        'Первый адрес', 
-        max_length=255, 
-        null=True
+class Address(models.Model):
+    raw_address = models.CharField('Адрес', max_length=255, unique=True)
+    latitude = models.DecimalField(
+        'Широта',
+        max_digits=9,
+        decimal_places=6,
+        null=True,
+        blank=True
     )
-    first_address_latitude = models.DecimalField(
-        'Широта первого адреса', 
-        max_digits=9, 
-        decimal_places=6, 
-        null=True
+    longitude = models.DecimalField(
+        'Долгота',
+        max_digits=9,
+        decimal_places=6,
+        null=True,
+        blank=True
     )
-    first_address_longitude = models.DecimalField(
-        'Долгота первого адреса', 
-        max_digits=9, 
-        decimal_places=6, 
-        null=True
-    )
-
-    second_address = models.CharField(
-        'Второй адрес', 
-        max_length=255, 
-        null=True
-    )
-    second_address_latitude = models.DecimalField(
-        'Широта второго адреса', 
-        max_digits=9, 
-        decimal_places=6, 
-        null=True
-    )
-    second_address_longitude = models.DecimalField(
-        'Долгота второго адреса', 
-        max_digits=9, 
-        decimal_places=6, 
-        null=True
-    )
-
-    distance = models.FloatField('Расстояние между адресами (км)', null=True, blank=True)
-
     last_updated = models.DateTimeField('Последнее обновление', auto_now=True)
 
-    class Meta:
-        unique_together = ('first_address', 'second_address')
-
     def __str__(self):
-        return (f"""{self.first_address} - ({self.first_address_latitude}, {self.first_address_longitude})
-                    {self.second_address} - ({self.second_address_latitude}, {self.second_address_longitude})""")
+        return f"{self.raw_address} ({self.latitude}, {self.longitude})"
